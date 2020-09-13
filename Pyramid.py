44263b4186bdf1245a1541f31d7a7ffdf39cd5d8
wfoody <foodyw@gmail.com>
2020-09-13 19:56:55 -0400

    updated pyramid project

--- Pyramid.py
+++ Pyramid.py
@@ -1,22 +1,5 @@
-#count = 1
-"""
-while (count < 18):
-    print("*"), count + 2
-    count = count + 2
-"""
-# For Loop (definite) vs While Loop (indefinite)
-# Start with one star on first line, then add another two stars for each new line (9)
 
-# Add the first 10 numbers starting from 0
-"""
-sum = 0
-for i in range(10):
-    sum += i
-print(sum)
-"""
 star = "*"
-# range(9) -> [0, 1, 2, 3, 4, 5, 6, 7, 8]
-# range(0, 9, 1)
 for line in range(9):
     # Print spaces
     # get the number of spaces we want to print
@@ -34,23 +17,3 @@ for line in range(9):
     # print output
     print(output)
 
-# Lines | Spaces | Equation
-# 0     | 8      | spaces = 8 - lines
-# 1     | 7      |
-# 2     | 6      |
-# 3     | 5      |
-# 4     | 4
-# 5     | 3
-# 6     | 2
-# 7     | 1
-# 8     | 0
-# Lines | Stars
-# 0     | 1
-# 1     | 3
-# 2     | 5
-# 3     | 7
-# 4     | 9
-# 5     | 11
-# 6     | 13
-# 7     | 15
-# 8     | 17
